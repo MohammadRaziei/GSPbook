54fae6a3c83a8cd6f0bbaa583e8c0ffdbb367bf9
MohammadRaziei <mohammadraziei1375@gmail.com>
2025-03-06 10:58:22 +0330

    create faBox, enBox

--- components/LangSwitch.tsx
+++ components/LangSwitch.tsx
@@ -1,24 +1,52 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef } from 'react';
 import styles from './../styles/page.module.css';
-import { 
-    // useTranslation, 
-    LanguageSwitcher, 
-    useLanguageQuery } from 'next-export-i18n';
+import { LanguageSwitcher, useLanguageQuery } from 'next-export-i18n';
 import ReactCountryFlag from 'react-country-flag';
 
 export const LangSwitch: React.FC = () => {
-//   const { t } = useTranslation();
   const [query] = useLanguageQuery();
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   const isRTL = query?.lang === 'fa';
+
+  const faBox = (
+    <LanguageSwitcher lang="fa">
+      <div className={styles.langOption}>
+        <ReactCountryFlag
+          countryCode="IR"
+          svg
+          style={{
+            width: '1.8em',
+            height: '1.8em',
+          }}
+        />
+        <span>فا</span>
+      </div>
+    </LanguageSwitcher>
+  );
+
+  const enBox = (
+    <LanguageSwitcher lang="en">
+      <div className={styles.langOption}>
+        <ReactCountryFlag
+          countryCode="US"
+          svg
+          style={{
+            width: '1.8em',
+            height: '1.8em',
+          }}
+        />
+        <span>EN</span>
+      </div>
+    </LanguageSwitcher>
+  );
 
   return (
     <div 
       className={`${styles.langSwitcherContainer} ${isRTL ? styles.rtl : ''}`} 
       ref={dropdownRef}
     >
-      <div className={styles.langSwitcherButton} style={{backgroundColor: 'inherit'}}>
+      <div className={styles.langSwitcherButton} style={{ backgroundColor: 'inherit' }}>
         <ReactCountryFlag
           countryCode={isRTL ? 'IR' : 'US'}
           svg
@@ -27,38 +55,23 @@ export const LangSwitch: React.FC = () => {
             height: '1.8em',
           }}
         />
-        <span style={{margin: '0.4em'}}>{isRTL ? 'فا' : 'EN'}</span>
+        <span style={{ margin: '0.4em' }}>{isRTL ? 'فا' : 'EN'}</span>
       </div>
       
       <div className={styles.langDropdown}>
-        <LanguageSwitcher lang="fa">
-          <div className={styles.langOption}>
-            <ReactCountryFlag
-              countryCode="IR"
-              svg
-              style={{
-                width: '1.8em',
-                height: '1.8em',
-              }}
-            />
-            <span>فا</span>
-          </div>
-        </LanguageSwitcher>
-        
-        <LanguageSwitcher lang="en">
-          <div className={styles.langOption}>
-            <ReactCountryFlag
-              countryCode="US"
-              svg
-              style={{
-                width: '1.8em',
-                height: '1.8em',
-              }}
-            />
-            <span>EN</span>
-          </div>
-        </LanguageSwitcher>
+        {/* Fixed the ternary operator syntax */}
+        {isRTL ? (
+          <>
+            {enBox}
+            {faBox}
+          </>
+        ) : (
+          <>
+            {faBox}
+            {enBox}
+          </>
+        )}
       </div>
     </div>
   );
-}; 
+};
